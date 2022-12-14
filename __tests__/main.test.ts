9387883cba0019c2ee2d2da1dc4c45179e7b4aae
0xKrauser <detroitmetalcrypto@gmail.com>
2022-12-14 15:47:23 +0000

    Feat: initial version

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,8 +2,29 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
+import Bundlr from '@bundlr-network/client'
 
-test('test runs', () => {
+test('initialize bundlr (expect success)', () => {
+  const endpoint = process.env['INPUT_BUNDLR-ENDPOINT']
+  const currency = process.env['INPUT_BUNDLR-CURRENCY']
+  const privateKey = process.env['INPUT_PRIVATE-KEY']
+  try {
+    const bundlr = new Bundlr(endpoint!, currency!, privateKey!)
+    expect(bundlr).toBeDefined()
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+test('initialize bundlr without private key (expect undefined node address)', () => {
+  let endpoint = process.env['INPUT_BUNDLR-ENDPOINT']!
+  let currency = process.env['INPUT_BUNDLR-CURRENCY']!
+  let privateKey = ''
+  const bundlr = new Bundlr(endpoint, currency, privateKey)
+  expect(bundlr.address).toBeUndefined()
+})
+
+test('test complete flow', () => {
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
